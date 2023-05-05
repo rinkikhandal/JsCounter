185f9ae4c35635bb0fbdf33a2dd591fc1e7d5a83
rinki khandal <rinkikhandal2000@gmail.com>
2023-05-05 20:22:37 +0530

    a change is made

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ const number = document.querySelector(".number");
 const inc = document.querySelector(".increase");
 const dec = document.querySelector(".decrease");
 const res = document.querySelector(".reset");
+
+res.addEventListener("click", function () {});
