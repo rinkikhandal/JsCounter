02a665220fa2de69aa792a7aef562531550d45c3
austereastute <rinkikhandal2000@gmail.com>
2023-05-05 22:42:03 +0530

    final result

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ const number = document.querySelector(".number");
 const inc = document.querySelector(".increase");
 const dec = document.querySelector(".decrease");
 const res = document.querySelector(".reset");
+
+let number1 = 0;
+
+res.addEventListener("click", function () {
+  number1 = 0;
+  number.textContent = number1;
+});
+
+inc.addEventListener("click", function () {
+  number1 += 1;
+  number.textContent = number1;
+});
+
+dec.addEventListener("click", function () {
+  number1 -= 1;
+  number.textContent = number1;
+});
