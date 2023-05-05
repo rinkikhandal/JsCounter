1030aeeac5e62cbdf75726c212002feba3c6564a
austereastute <rinkikhandal2000@gmail.com>
2023-05-06 00:30:08 +0530

    additional improvisations

--- index.js
+++ index.js
@@ -6,17 +6,32 @@ const res = document.querySelector(".reset");
 let number1 = 0;
 number.textContent = number1;
 
-res.addEventListener("click", function () {
-  number1 = 0;
-  number.textContent = number1;
-});
+// res.addEventListener("click", function () {
+//   number1 = 0;
+//   number.textContent = number1;
+// });
 
-inc.addEventListener("click", function () {
-  number1 += 1;
-  number.textContent = number1;
-});
+// inc.addEventListener("click", function () {
+//   number1 += 1;
+//   number.textContent = number1;
+// });
 
-dec.addEventListener("click", function () {
-  number1 -= 1;
-  number.textContent = number1;
+// dec.addEventListener("click", function () {
+//   number1 -= 1;
+//   number.textContent = number1;
+// });
+
+// or
+const btns = document.querySelectorAll("button");
+btns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    if (btn.classList.contains("increase")) {
+      number1++;
+    } else if (btn.classList.contains("decrease")) {
+      number1--;
+    } else {
+      number1 = 0;
+    }
+    number.textContent = number1;
+  });
 });
