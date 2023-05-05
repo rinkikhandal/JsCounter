a840eff23d8d89bc261853db6607b67cd0b522d4
rinki khandal <rinkikhandal2000@gmail.com>
2023-05-05 20:20:29 +0530

    this is my js project2

---
+++ index.js
@@ -0,0 +1,4 @@
+const number = document.querySelector(".number");
+const inc = document.querySelector(".increase");
+const dec = document.querySelector(".decrease");
+const res = document.querySelector(".reset");
